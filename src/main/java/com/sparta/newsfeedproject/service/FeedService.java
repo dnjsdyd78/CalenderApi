f649a52b7328538bff10144ed52b390ae8f6e5f8
goodsoundisme <sol000619@naver.com>
2024-09-05 11:58:07 +0900

    fix : 서비스부분 findByUserId 타입 수정 findAllByFollowingId 타입 수정

--- src/main/java/com/sparta/newsfeedproject/service/FeedService.java
+++ src/main/java/com/sparta/newsfeedproject/service/FeedService.java
@@ -13,6 +13,7 @@ import com.sparta.newsfeedproject.repository.FollowRepository;
 import com.sparta.newsfeedproject.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -20,6 +21,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -76,12 +78,13 @@ public class FeedService {
     //}피드 목록 조회
     public Page<FeedSimpleResponseDto> getFeeds(UserTokenDto tokenUser, int page) {
         //제공된 사용자 ID(id)를 기반으로 해당 사용자의 피드만을 조회하므로, 기본적으로 다른 사용자의 피드를 조회할 수 없음
+
         // ->로그인한 사용자와 요청된 id가 일치하는지 확인하는 로직을 추가하는게 좋을까..? 굳이 안해도 되나?
         User user = userRepository.findById(tokenUser.getUserId()).orElseThrow(() -> new NullPointerException("User ID가 존재하지 않습니다."));
 
         //10개씩 페이지네이션 : 각 페이지 당 뉴스피드 데이터가 10개씩 나오게 + 기본정렬이 생성일자 기준 내림차순
         PageRequest pageRequest = PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "createdAt"));
-        Page<Feed> feedPage = feedRepository.findByUserId(user, pageRequest);
+        Page<Feed> feedPage = feedRepository.findByUserId(user.getId(), pageRequest);
 
         //각 피드 엔티티를 DetailResponseDto로 변환해서 페이지 결과 반환
         return feedPage.map(feed -> new FeedSimpleResponseDto(
@@ -113,7 +116,7 @@ public class FeedService {
         User user = userRepository.findById(tokenUser.getUserId()).orElseThrow(()->new NullPointerException("user Id가 유효하지 않습니다."));
 
         //사용자가 팔로우한 사용자목록(팔로우목록) 조회
-        List<Follow> followList = followRepository.findAllByFollowingId(user);
+        List<Follow> followList = followRepository.findAllByFollowingId(user.getId());
         List<User> followedUsers = followList.stream()
                 .map(follow->follow.getFollower()) //<-follow 엔티티에서 팔로우 된 사용자 객체 가져오ㄹ ㅑ고
                 .collect(Collectors.toList());
