ae9b7bebd536717bf2dc96f8c55153f351f2af92
KUGOM <yaeji0728@naver.com>
2024-09-05 17:13:03 +0900

    Merge branch 'JeongYaeji' of https://github.com/newsfeed-project/newsfeed-project into dev
    # Conflicts:
    #	build.gradle
    #	src/main/java/com/sparta/newsfeedproject/controller/FollowController.java
    #	src/main/java/com/sparta/newsfeedproject/repository/FollowRepository.java
    #	src/main/java/com/sparta/newsfeedproject/repository/UserRepository.java
    #	src/main/java/com/sparta/newsfeedproject/service/FollowService.java

--- src/main/java/com/sparta/newsfeedproject/controller/GoodController.java
+++ src/main/java/com/sparta/newsfeedproject/controller/GoodController.java
@@ -5,6 +5,7 @@ import com.sparta.newsfeedproject.domain.Good;
 import com.sparta.newsfeedproject.dto.request.UserTokenDto;
 import com.sparta.newsfeedproject.service.GoodService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,13 +16,15 @@ public class GoodController {
     private GoodService goodService;
 
     @PostMapping("/good")
-    public void goodfeed(@Auth UserTokenDto userTokenDto, @RequestParam Long feedId) {
+    public ResponseEntity<String> goodfeed(@Auth UserTokenDto userTokenDto, @RequestParam Long feedId) {
         //사용자 ID, 피드ID로 좋아요 추가
         goodService.goodfeed(userTokenDto, feedId);
+        return ResponseEntity.ok("좋아요 완료");
     }
 
     @PostMapping("/ungood")
-    public void ungoodfeed(@Auth UserTokenDto userTokenDto, @RequestParam Long feedId) {
+    public ResponseEntity<String> ungoodfeed(@Auth UserTokenDto userTokenDto, @RequestParam Long feedId) {
         goodService.ungoodfeed(userTokenDto, feedId);//사용자 ID와 게시글 ID를 통해 좋아요 제거
+        return ResponseEntity.ok("좋아요 취소 완료");
     }
 }
--- src/main/java/com/sparta/newsfeedproject/service/GoodService.java
+++ src/main/java/com/sparta/newsfeedproject/service/GoodService.java
@@ -26,12 +26,12 @@ public class GoodService {
     @Transactional
     public void goodfeed(UserTokenDto userTokenDto, Long feedId){
         //사용자, 게시글 조회
-        User user = userRepository.findById(userTokenDto.getUserId()).orElseThrow(()->new IllegalArgumentException("User not found"));
-        Feed feed = feedRepository.findById(feedId).orElseThrow(()->new IllegalArgumentException("Feed not found"));
+        User user = userRepository.findById(userTokenDto.getUserId()).orElseThrow(()->new IllegalArgumentException("유저를 찾을 수 없습니다."));
+        Feed feed = feedRepository.findById(feedId).orElseThrow(()->new IllegalArgumentException("게시글을 찾을 수 없습니다."));
 
         //좋아요가 중복일 경우 예외처리
         if(goodRepository.existsByUserAndFeed(user, feed)){
-            throw new IllegalArgumentException("Post already liked");
+            throw new IllegalArgumentException("이미 좋아요를 누른 게시글입니다.");
         }
 
         //좋아요 기능
@@ -45,10 +45,10 @@ public class GoodService {
     //좋아요 삭제
     @Transactional
     public void ungoodfeed(UserTokenDto userTokenDto, Long feedId){
-        User user = userRepository.findById(userTokenDto.getUserId()).orElseThrow(()->new IllegalArgumentException("User not found"));
-        Feed feed = feedRepository.findById(feedId).orElseThrow(()->new IllegalArgumentException("Feed not found"));
+        User user = userRepository.findById(userTokenDto.getUserId()).orElseThrow(()->new IllegalArgumentException("유저를 찾을 수 없습니다."));
+        Feed feed = feedRepository.findById(feedId).orElseThrow(()->new IllegalArgumentException("게시글을 찾을 수 없습니다."));
 
-        Good good = goodRepository.findByUserAndFeed(user, feed).orElseThrow(()->new IllegalArgumentException("Good not found"));
+        Good good = goodRepository.findByUserAndFeed(user, feed).orElseThrow(()->new IllegalArgumentException("좋아요 한 게시글이 아닙니다."));
 
         goodRepository.delete(good);
     }
